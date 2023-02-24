d4920c8abc595683a6fe8e3c3427f2714289f01d
Klaus <kriegle@seznam.cz>
2023-02-24 11:37:07 +0100

    owner info

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OwnerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/owners/{id}', [OwnerController::class, 'detail'])->name('owners.detail');
+Route::get('/owner', [OwnerController::class, 'results'])->name('owners.result');
