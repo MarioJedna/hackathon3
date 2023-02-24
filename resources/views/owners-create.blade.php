5b6c1820f35ce5a2bd4ce1c650c5f438046b639c
Kurt Rode <kurtrode@seznam.cz>
2023-02-24 14:41:03 +0100

    added owners create

---
+++ resources/views/owners-create.blade.php
@@ -0,0 +1,15 @@
+@include('common.html-start-1')
+<form action="" method=GET>
+<label for="name">Enter First Name: </label>
+<input type="text" name="name"><br>
+<label for="surname">Enter Last Name: </label>
+<input type="text" name="surname"><br>
+<label for="email">Enter E-Mail: </label>
+<input type="email" name="email"><br>
+<label for="phone">Enter Phone Number: </label>
+<input type="text" name="phone"><br>
+<label for="address">Enter Address: </label>
+<input type="text" name="address"><br>
+<button>Submit</button>
+</form>
+@include('common.html-end')
